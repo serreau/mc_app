5602275fb02929cffe06828ee125d1fcea99cc9b
serreau <serreau.a@gmail.com>
2019-07-10 23:34:37 +0200

    database as a singleton

--- app/src/main/java/sero/com/databases/NanosDatabase.java
+++ app/src/main/java/sero/com/databases/NanosDatabase.java
@@ -1,7 +1,9 @@
 package sero.com.databases;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import sero.com.dao.NanosDao;
 import sero.com.entities.Nanos;
@@ -10,4 +12,17 @@ import sero.com.entities.Nanos;
 public abstract class NanosDatabase extends RoomDatabase {
 
     public abstract NanosDao nanosDao();
+
+    private static NanosDatabase INSTANCE;
+
+    static NanosDatabase getInstance(final Context context) {
+        synchronized (NanosDatabase.class) {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        NanosDatabase.class, "nanos_database")
+                        .build();
+            }
+        }
+        return INSTANCE;
+    }
 }
