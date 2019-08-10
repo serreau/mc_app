24656d7c0cc4709189c4a368ff103e6812cdf495
serreau <serreau.a@gmail.com>
2019-08-10 18:11:51 +0200

    Permission WRITE/READ, Profile Image, ConstraintLayout

--- app/src/main/java/sero/com/util/FileManager.java
+++ app/src/main/java/sero/com/util/FileManager.java
@@ -8,20 +8,4 @@ public final class FileManager {
     public  static Uri getProfilImage() {
         return Uri.parse(Environment.getExternalStorageDirectory().toString() + "/profile.png");
     }
-
-
-
-    /*public static Uri chargeProfilImage(Uri uri) throws FileNotFoundException {
-        String profile = Environment.getExternalStorageDirectory().toString() + "/profile.png";
-        FileOutputStream out = new FileOutputStream(profile);
-
-        File f = new File(uri.getPath());
-        InputStream in = new FileInputStream(f);
-        Bitmap b = BitmapFactory.decodeStream(in);
-
-        b.compress(Bitmap.CompressFormat.PNG, 100, out);
-
-        File tmp = new File(out.toString());
-        return Uri.fromFile(tmp);
-    }*/
 }
